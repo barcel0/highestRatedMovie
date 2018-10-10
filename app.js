7a10c5cf92b254d4f064ad2f2e9a6eeced46ba20
Luis <lbarcelocarrera@gmail.com>
2018-10-10 12:48:17 +0200

    Initial Code
    //Promise.all - Gets highest rated movie (Rotten Tomatoes) from OMDB API
    
    Bug: When two movies have the same highest score, it only displays the first one in the map

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
+//Promise.all - Gets highest rated movie (Rotten Tomatoes) from OMDB API
 
+function getBestMovie(...movies){
+  const apiKey = 'Your OMDB API key';
+  const baseUrl = `http://www.omdbapi.com/?apikey=${apiKey}&t=`,
+        urls = movies.map( title => $.getJSON(baseUrl + title) );
+
+  return Promise.all(urls)
+    .then(formatRatings)
+    .then(getHighestRating)
+    .catch(err=>console.log(err));
+}
+
+function formatRatings(data){ 
+ //Convert ratings to int and store them in arr
+ //Create map with movie title and int rating
+  const ratingsMap = new Map();
+    data.forEach(function(movie){
+      let formattedRatting = parseInt(movie.Ratings[1].Value.replace('%',''));
+      ratingsMap.set(movie.Title, formattedRatting);
+    });
+  
+ //New Map in ascending order
+  const ratingsMapAsc = new Map([...ratingsMap.entries()].sort((a,b)=>b[1]-a[1]));  
+  return ratingsMapAsc;
+}
+
+function getHighestRating(movieMap){
+  //get map's first element
+  const movie = movieMap.keys().next().value,
+        rating = movieMap.values().next().value;
+  console.log(`Highest rated movie is ${movie} with a rating of ${rating}% in the Tomatometer.`);
+  };
+
+//testing
+getBestMovie('die hard', 'jumanji', 'blade runner', 'requiem for a dream');
